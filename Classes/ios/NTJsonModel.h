6fec82a713a0b2c9832f2a97af23e00994c741ba
Ethan Nagel <eanagel@gmail.com>
2014-10-22 18:17:29 -0700

    WIP documentation

--- Classes/ios/NTJsonModel.h
+++ Classes/ios/NTJsonModel.h
@@ -18,21 +18,88 @@
 
 @interface NTJsonModel : NSObject <NSCopying, NSMutableCopying>
 
+/**
+ *  returns YES if this is a mutable instance
+ */
 @property (nonatomic,readonly) BOOL isMutable;
+
+
+/**
+ *  returns the default JSON for this object.
+ *
+ *  @return NSDictionary with the default JSON for this object.
+ */
 +(NSDictionary *)defaultJson;
 
+
+/**
+ *  returns the JSON representation of the object
+ *
+ *  @return NSDictionary with the JSON representation of the object.
+ */
 -(NSDictionary *)asJson;
 
 +(Class)modelClassForJson:(NSDictionary *)json;
 +(BOOL)modelClassForJsonOverridden;
 
+/**
+ *  returns a default mutable instance.
+ */
 -(id)init; // creates mutable instance
+
+
+/**
+ *  returns an immutable object with the supplied JSON
+ *
+ *  @param json the JSON
+ *
+ *  @return a new immutable model instance
+ */
 -(id)initWithJson:(NSDictionary *)json;
+
+/**
+ *  returns an mutable object with the supplied JSON
+ *
+ *  @param json the JSON
+ *
+ *  @return a new mutable model instance
+ */
 -(id)initMutableWithJson:(NSDictionary *)json;
+
+/**
+ *  returns an immutable object with the supplied JSON or nil if json is nil
+ *
+ *  @param json the JSON
+ *
+ *  @return a new immutable model instance or nil
+ */
 +(instancetype)modelWithJson:(NSDictionary *)json;
+
+/**
+ *  returns an mutable object with the supplied JSON or nil if json is nil
+ *
+ *  @param json the JSON
+ *
+ *  @return a new mutable model instance or nil
+ */
 +(instancetype)mutableModelWithJson:(NSDictionary *)json;
 
+/**
+ *  returns an array of immutable Model objects with the supplied type. Objects are created lazily as they are accessed.
+ *
+ *  @param jsonArray the JSON array
+ *
+ *  @return an array of Model objects.
+ */
 +(NSArray *)arrayWithJsonArray:(NSArray *)jsonArray;
+
+/**
+ *  returns an array of mutable Model objects with the supplied type. Objects are created lazily as they are accessed.
+ *
+ *  @param jsonArray the JSON array
+ *
+ *  @return a mutable array of mutable Model objects.
+ */
 +(NSMutableArray *)mutableArrayWithJsonArray:(NSArray *)jsonArray;
 
 -(id)copyWithZone:(NSZone *)zone;
@@ -43,6 +110,12 @@
 -(NSUInteger)hash;
 
 -(NSString *)description;
+
+/**
+ *  returns a detailed description of the object
+ *
+ *  @return a detailed description of the object
+ */
 -(NSString *)fullDescription;
 
 @end
